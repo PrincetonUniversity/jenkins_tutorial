29020039af3efe06dc247593bb6270a18cfaaa89
David Luet <luet@princeton.edu>
2016-03-25 10:23:37 -0400

    Initial commit

---
+++ factorial.py
@@ -0,0 +1,14 @@
+#! /bin/env python
+import sys
+
+def recur_factorial(n):
+   """Function to return the factorial
+   of a number using recursion"""
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+if __name__ == '__main__':
+    arg = sys.argv
+    print recur_factorial(int(arg[1]))
