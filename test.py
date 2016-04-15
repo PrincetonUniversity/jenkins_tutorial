ee6607fb58071b1daf6612d4ab329ea746899476
David Luet <luet@princeton.edu>
2016-04-15 12:44:45 -0400

    Add a unit test on known value

---
+++ test.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import unittest
+from factorial import recur_factorial
+
+class TestReturnValues(unittest.TestCase):
+    def test_value(self):
+        """
+        Test for a known return value
+        """
+        self.assertEqual(recur_factorial(3), 6)
+
+if __name__  == '__main__':
+    unittest.main()
